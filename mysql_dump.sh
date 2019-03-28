154278b41d3ab28170dfaace63a7fd2e1105df1b
mygui <guimingyue@gmail.com>
2019-03-28 19:56:09 +0800

    Update mysql_dump.sh

--- mysql_dump.sh
+++ mysql_dump.sh
@@ -2,9 +2,9 @@
 mysqldump -uroot -pdbpasswd -d dbname >db.sql;
 # 导出 dbname 库 test 表结构
 mysqldump -uroot -pdbpasswd -d dbname test>db.sql;
-# 导出 dbname 库所有表结构及数据（不加-d）
+# 导出 dbname 库所有表结构及<pre>数据</pre>（不加-d）
 mysqldump -uroot -pdbpasswd  dbname >db.sql;
-# 导出 dbname 库的表 test 表结构及数据（不加-d）
+# 导出 dbname 库的表 test 表结构及<pre>数据</pre>（不加-d）
 mysqldump -uroot -pdbpasswd dbname test>db.sql;
 
 mysqldump -h -P3306 -u -p dbName tableName --where=""
